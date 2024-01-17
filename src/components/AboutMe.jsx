49bd9f4dd2f8bbe3f398eda478f146ce8dd1439a
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-17 14:46:04 +1100

    Created basic resume & aboutme pages

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,19 @@
 function AboutMe(){
     return(
         <div>
-            About Me  ------   Welcome to the jungle
+            <h1>About Me</h1>
+            <img src="/cat.jpg" alt="gamer cat" width={140}/>
             <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sunt rem inventore doloribus, odio, distinctio maiores natus, rerum dolore officiis hic commodi consectetur id unde quidem adipisci excepturi consequatur blanditiis.
-            </div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, quos ex? Minima sunt eligendi eos enim quisquam iusto distinctio praesentium beatae ab pariatur. Quia, itaque nemo dolore doloremque ducimus voluptatibus?
+                Saepe eveniet exercitationem consequatur consequuntur cumque praesentium vitae eaque cum molestias dolorem autem accusamus aliquid, laudantium esse porro quas? Aspernatur sapiente maiores enim temporibus tenetur illo, fugiat consectetur cumque in?
+                Exercitationem impedit rem eius ipsum, sapiente aspernatur itaque modi. Distinctio dolorem nulla, est et, veritatis, quae odit autem commodi eaque perferendis nesciunt mollitia eius dignissimos aliquid rerum harum modi in.
+                Repellendus, tenetur placeat eos nihil velit ratione cupiditate expedita tempora consequuntur veniam, enim nesciunt a unde esse laudantium? Facere obcaecati quia magni blanditiis? Et, aperiam voluptates ut harum iste officiis?
+                Cum nam repudiandae ab cumque facere corrupti voluptatum? Recusandae unde quasi qui! Corrupti ab saepe, esse iusto commodi veritatis voluptas, a cumque unde et aliquam eos delectus. Animi, aperiam alias!
+                Alias cupiditate, itaque voluptates atque placeat laboriosam dolorum rerum libero, officia, iusto doloremque. Vero nostrum architecto, consequuntur aspernatur repudiandae tempora dolore exercitationem minus, quo repellat tenetur aut quasi recusandae voluptate!
+                Reiciendis, molestiae repudiandae. Similique qui dolor quisquam tenetur cupiditate obcaecati totam? Dolores recusandae debitis nostrum dolorum maxime at non tempore a in, placeat aspernatur sequi eligendi. Aspernatur nobis totam eligendi.
+                Dolorum placeat, odio atque suscipit repellendus similique illo nulla cumque molestiae, ipsum voluptatibus? Maiores et itaque repudiandae nulla numquam tenetur quidem odio similique earum, necessitatibus quo ipsa eligendi! Pariatur, est?
+                Voluptatibus ratione optio repudiandae ad ullam nostrum exercitationem voluptatum consectetur sed distinctio provident eaque illum, dolorum consequatur dolor saepe repellendus magnam ipsam id odit ex minima, tenetur doloribus suscipit? Laboriosam!
+                Voluptatibus consectetur ullam, dolores libero, ipsa, itaque sint molestias ad doloremque corporis non dolorem laborum et ex natus laudantium maiores numquam mollitia aperiam nam voluptatem quod blanditiis. Maiores, nihil asperiores?            </div>
         </div>
     )
 };
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,21 @@
+import List from './List';
+
+
 function Resume(){
-    return(
-        <div>Resume Page</div>
+
+    const skillsFront = ["Javascript", "CSS", "HTML"]
+    const skillsBack = ["MongoDB", "MySQL2", "React"];
+
+
+
+    return (
+        <div>
+            <h2>Resume Page </h2>
+            <h3>Front End Proficiencies</h3>
+            <List skills={skillsFront} idName={"frontList"}/>
+            <h3>Back End Proficiencies</h3>
+            <List skills={skillsBack} idName={"backList"}/>
+        </div>
     )
 }
 
