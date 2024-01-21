cb34db657533799f9665d2d7e08946385b6797a8
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-22 08:52:29 +1100

    worked on header styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Header-Footer.css'
 
 function Footer(){
     return(
-        <div className="footer" style={{background: 'blue'}}>
+        <div className="footer" >
             <a href="http://https://github.com/rtrevill"><img src="/github-9-128.png" alt="GitLogo" className="footerLogo" /></a>
             <a href="https://stackexchange.com/users/30366917/rtrevill"><img src="/stackoverflow-128.png" alt="StackOverflow Logo" className="footerLogo"/></a>
             <a href="https://www.linkedin.com/in/richard-trevillian-5a636820/"><img src="/linkedin-2-128.png" alt="linkedIn Logo" className="footerLogo"/></a>
