0de93e153daeb6ff6fb593cea8b22d40411faa18
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-22 21:54:57 +1100

    Pushing to Heroku

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://rtrevill-modern-portfolio-09375a52d946.herokuapp.com/',
   plugins: [react()],
   server: {
     port: process.env.PORT||3000,
