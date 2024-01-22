dfe28ae6844d2952aa09e9be80a9a20395dd73a0
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-22 21:24:38 +1100

    abc

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://rtrevill-modern-portfolio-09375a52d946.herokuapp.com/',
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   open: true
-  // }
+  server: {
+    port: 3000,
+    open: true
+  }
 })
