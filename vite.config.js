1ab01cd3a83a432f6724249faa830c97a30a187d
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-23 09:22:11 +1100

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+let PORT = process.env.PORT || 3000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT||3000,
+    port: PORT,
     open: true
   }
 })
